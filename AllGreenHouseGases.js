e59fc9d875fb6d6b3426d399010fc2c69a021bf6
dbharathkumar08 <dbharathkumar08@gmail.com>
2016-11-28 00:55:39 -0500

    Adding drop down and select for all gas types

--- AllGreenHouseGases.js
+++ AllGreenHouseGases.js
@@ -282,3 +282,57 @@ function draw_Methane(place){
             .call(yAxis);
     });
 }
+
+
+$(document).ready(function(){
+    $('#purpose').on('change', function() {
+        if ( this.value == '2')
+        {
+            $("#No2_graph").show();
+        }
+        else
+        {
+            $("#No2_graph").hide();
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#purpose').on('change', function() {
+        if ( this.value == '1')
+        {
+            $("#Co2_graph").show();
+        }
+        else
+        {
+            $("#Co2_graph").hide();
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#purpose').on('change', function() {
+        if ( this.value == '3')
+        {
+            $("#Methane_graph").show();
+        }
+        else
+        {
+            $("#Methane_graph").hide();
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('#purpose').on('change', function() {
+        if ( this.value == '0')
+        {
+            $("#Methane_graph").show();
+            $("#Co2_graph").show();
+            $("#No2_graph").show();
+        }
+        else
+        {
+        }
+    });
+});
