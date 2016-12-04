1c1ec820fa176a5c87676085b1b00d04b0eeae5c
dbharathkumar08 <dbharathkumar08@gmail.com>
2016-12-03 21:15:03 -0500

    Adding Line Graphs and rescaling y axis to a range between 0-100.- Fixing load time errors

--- AllGreenHouseGases.js
+++ AllGreenHouseGases.js
@@ -14,119 +14,37 @@ function valueFormat(d,city) {
     return k;
 }
 
-function draw_NO2(place){
-    parent.document.getElementById("No2_graph").innerHTML = "";
-    // window.alert(place);
-// Set the dimensions of the canvas / graph
-    var margin = {top: 50, right: 20, bottom: 30, left: 100},
-        width = 900 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
-// Parse the date / time
-// Set the ranges
-    var x = d3.scale.linear().range([0, width]);
-    var y = d3.scale.linear().range([height, 0]);
-    var formatyAxis = d3.format('.0f');
-// Define the axes
-    var xAxis = d3.svg.axis().scale(x)
-        .orient("bottom").ticks(15);
-    var yAxis = d3.svg.axis().scale(y)
-        .orient("left")
-        .tickFormat(formatyAxis)
-        .ticks(5);
-// Define the line
-    var valueline = d3.svg.line()
-        .x(function(d) { return x(d.Year); })
-        .y(function(d) { return y(d[place]); });
-// Define the div for the tooltip
-    var div = d3.select("#No2_graph").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
-// Adds the svg canvas
-    var svg = d3.select("#No2_graph")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-// Get the data
 
 
 
-    d3.csv("NO2Data.csv", function(error, data) {
-
-        data.forEach(function(d) {
-
-            d.Year = d.Year;
-            d[place] = + d[place];
-        });
-
-
-// Scale the range of the data
-        x.domain(d3.extent(data, function(d) { return d.Year; }));
-        y.domain([0, d3.max(data, function(d) { return d[place]; })]);
-
-        console.log(d3.max(data, function(d) { return d[place]; }));
-// Add the valueline path.
-        svg.append("path")
-            .attr("class", "line")
-            .attr("d", valueline(data));
-// Add the scatterplot
-        svg.selectAll("dot")
-            .data(data)
-            .enter().append("circle")
-            .attr("r", 3)
-            .attr("cx", function(d) { return x(d.Year); })
-            .attr("cy", function(d) { return y(d[place]); })
-            .on("mouseover", function(d) {
-                div.transition()
-                    .duration(200)
-                    .style("opacity", .9);
-                div .html("Year: "+(d.Year) + "<br/> NO2 Emission: "  + valueFormat(d[place]))
-                    .style("left", (d3.event.pageX) + "px")
-                    .style("top", (d3.event.pageY - 28) + "px");
-            })
-            .on("mouseout", function(d) {
-                div.transition()
-                    .duration(500)
-                    .style("opacity", 0);
-            });
-// Add the X Axis
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-// Add the Y Axis
-        svg.append("g")
-            .attr("class", "y axis")
-            .call(yAxis);
-    });
-}
-
-
-function draw_CO2(place){
+function draw_Co2(place){
     parent.document.getElementById("Co2_graph").innerHTML = "";
     // window.alert(place);
+    console.log(place);
 // Set the dimensions of the canvas / graph
-    var margin = {top: 50, right: 20, bottom: 30, left: 100},
+    var place2 = "World";
+    var margin = {top: 30, right: 20, bottom: 30, left: 100},
         width = 900 - margin.left - margin.right,
         height = 200 - margin.top - margin.bottom;
 // Parse the date / time
 // Set the ranges
     var x = d3.scale.linear().range([0, width]);
     var y = d3.scale.linear().range([height, 0]);
-    var formatyAxis = d3.format('.0f');
 // Define the axes
     var xAxis = d3.svg.axis().scale(x)
         .orient("bottom").ticks(15);
     var yAxis = d3.svg.axis().scale(y)
-        .orient("left")
-        .tickFormat(formatyAxis)
-        .ticks(5);
+        .orient("left").ticks(5);
 // Define the line
     var valueline = d3.svg.line()
         .x(function(d) { return x(d.Year); })
         .y(function(d) { return y(d[place]); });
+
+    // Define the line
+    var valueline2 = d3.svg.line()
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d[place2]); });
+
 // Define the div for the tooltip
     var div = d3.select("#Co2_graph").append("div")
         .attr("class", "tooltip")
@@ -145,23 +63,53 @@ function draw_CO2(place){
 
     d3.csv("CO2Data.csv", function(error, data) {
 
+        var max = + d3.max(data, function(d) { return + d[place]; });
+        var min = + d3.min(data, function(d) { return + d[place]; });
+
+        var maxWorld = + d3.max(data, function(d) { return + d[place2]; });
+        var minWorld = + d3.min(data, function(d) { return + d[place2]; });
+
+        console.log(max);
+        console.log(min);
+
+        console.log(maxWorld);
+        console.log(minWorld);
+
+        var offset = + 100/ (max - min);
+        var offsetWorld = + 100/ (maxWorld - minWorld);
+
+        console.log(offset);
+
+        console.log(offsetWorld);
         data.forEach(function(d) {
 
+            console.log(d[place]);
+            console.log(d[place2]);
             d.Year = d.Year;
-            d[place] = + d[place];
+            d[place] = + ((+ (+ d[place] - max) * offset)) + 100;
+            console.log(d[place]);
+            if(place!="World"){
+                d[place2] = + ((+ (+ d[place2] - maxWorld) * offsetWorld)) + 100;
+                console.log(d[place2]);
+            }
+
         });
-
-
 // Scale the range of the data
         x.domain(d3.extent(data, function(d) { return d.Year; }));
         y.domain([0, d3.max(data, function(d) { return d[place]; })]);
-
-        console.log(d3.max(data, function(d) { return d[place]; }));
 // Add the valueline path.
         svg.append("path")
-            .attr("class", "line")
+            .attr("class", "line lineClass")
             .attr("d", valueline(data));
+
+        svg.append("path")
+            .attr("class", "line lineClass")
+            .style("stroke", "red")
+            .attr("d", valueline2(data));
 // Add the scatterplot
+
+
+
         svg.selectAll("dot")
             .data(data)
             .enter().append("circle")
@@ -172,7 +120,28 @@ function draw_CO2(place){
                 div.transition()
                     .duration(200)
                     .style("opacity", .9);
-                div .html("Year: "+(d.Year) + "<br/> CO2 Emission: "  + valueFormat(d[place]))
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place]))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 3)
+            .attr("cx", function(d) { return x(d.Year); })
+            .attr("cy", function(d) { return y(d[place2]); })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place2]))
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY - 28) + "px");
             })
@@ -190,33 +159,43 @@ function draw_CO2(place){
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis);
-    });
-}
 
+
+    });
+
+
+
+
+}
 
 function draw_Methane(place){
     parent.document.getElementById("Methane_graph").innerHTML = "";
     // window.alert(place);
+    console.log(place);
 // Set the dimensions of the canvas / graph
-    var margin = {top: 50, right: 20, bottom: 30, left: 100},
+    var place2 = "World";
+    var margin = {top: 30, right: 20, bottom: 30, left: 100},
         width = 900 - margin.left - margin.right,
         height = 200 - margin.top - margin.bottom;
 // Parse the date / time
 // Set the ranges
     var x = d3.scale.linear().range([0, width]);
     var y = d3.scale.linear().range([height, 0]);
-    var formatyAxis = d3.format('.0f');
 // Define the axes
     var xAxis = d3.svg.axis().scale(x)
         .orient("bottom").ticks(15);
     var yAxis = d3.svg.axis().scale(y)
-        .orient("left")
-        .tickFormat(formatyAxis)
-        .ticks(5);
+        .orient("left").ticks(5);
 // Define the line
     var valueline = d3.svg.line()
         .x(function(d) { return x(d.Year); })
         .y(function(d) { return y(d[place]); });
+
+    // Define the line
+    var valueline2 = d3.svg.line()
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d[place2]); });
+
 // Define the div for the tooltip
     var div = d3.select("#Methane_graph").append("div")
         .attr("class", "tooltip")
@@ -235,23 +214,53 @@ function draw_Methane(place){
 
     d3.csv("MethaneDataT.csv", function(error, data) {
 
+        var max = + d3.max(data, function(d) { return + d[place]; });
+        var min = + d3.min(data, function(d) { return + d[place]; });
+
+        var maxWorld = + d3.max(data, function(d) { return + d[place2]; });
+        var minWorld = + d3.min(data, function(d) { return + d[place2]; });
+
+        console.log(max);
+        console.log(min);
+
+        console.log(maxWorld);
+        console.log(minWorld);
+
+        var offset = + 100/ (max - min);
+        var offsetWorld = + 100/ (maxWorld - minWorld);
+
+        console.log(offset);
+
+        console.log(offsetWorld);
         data.forEach(function(d) {
 
+            console.log(d[place]);
+            console.log(d[place2]);
             d.Year = d.Year;
-            d[place] = + d[place];
+            d[place] = + ((+ (+ d[place] - max) * offset)) + 100;
+            console.log(d[place]);
+            if(place!="World"){
+                d[place2] = + ((+ (+ d[place2] - maxWorld) * offsetWorld)) + 100;
+                console.log(d[place2]);
+            }
+
         });
-
-
 // Scale the range of the data
         x.domain(d3.extent(data, function(d) { return d.Year; }));
         y.domain([0, d3.max(data, function(d) { return d[place]; })]);
-
-        console.log(d3.max(data, function(d) { return d[place]; }));
 // Add the valueline path.
         svg.append("path")
-            .attr("class", "line")
+            .attr("class", "line lineClass")
             .attr("d", valueline(data));
+
+        svg.append("path")
+            .attr("class", "line lineClass")
+            .style("stroke", "red")
+            .attr("d", valueline2(data));
 // Add the scatterplot
+
+
+
         svg.selectAll("dot")
             .data(data)
             .enter().append("circle")
@@ -262,7 +271,28 @@ function draw_Methane(place){
                 div.transition()
                     .duration(200)
                     .style("opacity", .9);
-                div .html("Year: "+(d.Year) + "<br/> CO2 Emission: "  + valueFormat(d[place]))
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place]))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 3)
+            .attr("cx", function(d) { return x(d.Year); })
+            .attr("cy", function(d) { return y(d[place2]); })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place2]))
                     .style("left", (d3.event.pageX) + "px")
                     .style("top", (d3.event.pageY - 28) + "px");
             })
@@ -280,11 +310,164 @@ function draw_Methane(place){
         svg.append("g")
             .attr("class", "y axis")
             .call(yAxis);
+
+
     });
+
+
+
+
 }
+function draw_NO2(place){
+    parent.document.getElementById("No2_graph").innerHTML = "";
+    // window.alert(place);
+    console.log(place);
+// Set the dimensions of the canvas / graph
+    var place2 = "World";
+    var margin = {top: 30, right: 20, bottom: 30, left: 100},
+        width = 900 - margin.left - margin.right,
+        height = 200 - margin.top - margin.bottom;
+// Parse the date / time
+// Set the ranges
+    var x = d3.scale.linear().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+// Define the axes
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(15);
+    var yAxis = d3.svg.axis().scale(y)
+        .orient("left").ticks(5);
+// Define the line
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d[place]); });
+
+    // Define the line
+    var valueline2 = d3.svg.line()
+        .x(function(d) { return x(d.Year); })
+        .y(function(d) { return y(d[place2]); });
+
+// Define the div for the tooltip
+    var div = d3.select("#No2_graph").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+// Adds the svg canvas
+    var svg = d3.select("#No2_graph")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+// Get the data
 
 
 
+    d3.csv("NO2Data.csv", function(error, data) {
+
+        var max = + d3.max(data, function(d) { return + d[place]; });
+        var min = + d3.min(data, function(d) { return + d[place]; });
+
+        var maxWorld = + d3.max(data, function(d) { return + d[place2]; });
+        var minWorld = + d3.min(data, function(d) { return + d[place2]; });
+
+        console.log(max);
+        console.log(min);
+
+        console.log(maxWorld);
+        console.log(minWorld);
+
+        var offset = + 100/ (max - min);
+        var offsetWorld = + 100/ (maxWorld - minWorld);
+
+        console.log(offset);
+
+        console.log(offsetWorld);
+        data.forEach(function(d) {
+
+            console.log(d[place]);
+            console.log(d[place2]);
+            d.Year = d.Year;
+            d[place] = + ((+ (+ d[place] - max) * offset)) + 100;
+            console.log(d[place]);
+            if(place!="World"){
+                d[place2] = + ((+ (+ d[place2] - maxWorld) * offsetWorld)) + 100;
+                console.log(d[place2]);
+            }
+
+        });
+// Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.Year; }));
+        y.domain([0, d3.max(data, function(d) { return d[place]; })]);
+// Add the valueline path.
+        svg.append("path")
+            .attr("class", "line lineClass")
+            .attr("d", valueline(data));
+
+        svg.append("path")
+            .attr("class", "line lineClass")
+            .style("stroke", "red")
+            .attr("d", valueline2(data));
+// Add the scatterplot
+
+
+
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 3)
+            .attr("cx", function(d) { return x(d.Year); })
+            .attr("cy", function(d) { return y(d[place]); })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place]))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+
+
+        svg.selectAll("dot")
+            .data(data)
+            .enter().append("circle")
+            .attr("r", 3)
+            .attr("cx", function(d) { return x(d.Year); })
+            .attr("cy", function(d) { return y(d[place2]); })
+            .on("mouseover", function(d) {
+                div.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                div .html("Year: "+(d.Year) + "<br/> GreenHouse: "  + valueFormat(d[place2]))
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                div.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+// Add the X Axis
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+// Add the Y Axis
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+
+    });
+
+
+
+
+}
 
 
 $(function () {
