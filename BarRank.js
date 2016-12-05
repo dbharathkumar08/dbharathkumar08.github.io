6b84dbce3a346e90b36025a09d5d596ef57bf71d
skillet urs <skillet urs>
2016-12-05 17:24:16 -0500

    fixed slider

--- BarRank.js
+++ BarRank.js
@@ -40,9 +40,9 @@ d3.csv("coda.csv", function(error, data) {
     d[year] = +d[year];
   });
     //data.filter(function(d) { return d[year] > 20,000,000 });
-    y.domain(data.sort(true? function(a, b) { return b[year] - a[year]; }: function(a, b) { return d3.ascending(a.Code, b.Code); })
-        .slice(0,50)
-        .map(function(d) { console.log(d.Code);return d.Code; }));
+    y.domain(data.sort(true? function(a, b) { return b[year] - a[year]; }: function(a, b) { return d3.ascending(a.Country, b.Country); })
+        .slice(0,20)
+        .map(function(d) {return d.Country; }));
   x.domain([0, d3.max(data, function(d) { return d[year]; })]);
 
   svg.append("g")
@@ -59,7 +59,7 @@ d3.csv("coda.csv", function(error, data) {
       .data(data)
     .enter().append("rect")
       .attr("class", "bar")
-      .attr("y", function(d) { return y(d.Code); })
+      .attr("y", function(d) { return y(d.Country); })
       .attr("height", y.rangeBand())
       .attr("x", function(d) { return x(d[year]); })
       .attr("width", function(d) { return height - x(d[year]); });
